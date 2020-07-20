0ac5760dfb3f94d888536a363f602c2bc37d6fe9
Pablo Rausch <pablorausch@gmail.com>
2020-07-20 09:42:47 -0300

    audit fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { RecorridosComponent } from './features/recorridos/recorridos/recorridos.component';
-import { RecorridosEditComponent } from './features/recorridos/recorridos-edit/recorridos-edit.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { RecorridosComponent } from "./features/recorridos/recorridos/recorridos.component";
+import { RecorridosEditComponent } from "./features/recorridos/recorridos-edit/recorridos-edit.component";
 
 const routes: Routes = [
-  {path: '', redirectTo: '/recorridos', pathMatch: 'full'},
-  {path: 'recorridos-edit/:id', component:RecorridosEditComponent}
+  { path: "", redirectTo: "/recorridos", pathMatch: "full" },
+  { path: "recorridos-edit/:id", component: RecorridosEditComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
